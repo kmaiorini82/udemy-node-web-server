d8a7a9970f7dbccd6ce228af9953b15716e104db
KJM <kmaiorini@gmail.com>
2019-01-24 11:18:38 -0500

    Added projects page

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+        welcomeMessage: 'Here are my projects'
+    });
+});
+
 app.get('/about',(req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
